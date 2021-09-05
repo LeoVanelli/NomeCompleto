45d2143603bdac2c0f40c0d6419b058dd253a7df
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-05 16:46:31 -0300

    Adiciona atividade NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName;
+            Console.Write("Digite seu nome: "); // Digita o primeiro nome do usuário
+            firstName = Console.ReadLine();
+            Console.Write("Digite seu sobrenome: "); // Digita o sobrenome do usuário
+            lastName = Console.ReadLine();
+
+            // ------------------
+
+            Console.WriteLine($"Seu nome é {firstName} {lastName}"); // Escreve o nome digitado
+            Console.WriteLine($"Nome de catálogo: {lastName.ToUpper()}, {firstName}"); // Escreve o nome catalogado
+        }
+    }
+}
